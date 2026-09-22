b0c6090367101bab30b484900cec218d5df88c62
Vo Minh Nghia <vominhnghia1878@gmail.com>
2026-09-22 23:59:35 +0700

    perf: add lean goal option type

--- apps/web/features/goals/goal.types.ts
+++ apps/web/features/goals/goal.types.ts
@@ -2,6 +2,8 @@ import type { Database } from "@/types/database.types";
 
 export type Goal = Database["public"]["Tables"]["goals"]["Row"];
 
+export type GoalOption = Pick<Goal, "id" | "title">;
+
 export type GoalWithProgress = Goal & {
   total_tasks: number;
   completed_tasks: number;
